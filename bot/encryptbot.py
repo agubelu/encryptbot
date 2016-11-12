5cb049b010c6a9bbe82207609f547bf1e3791abc
agubelu <agu@pqqqqw.pw>
2016-11-12 23:22:16 +0100

    Command create-domain working

--- bot/encryptbot.py
+++ bot/encryptbot.py
@@ -1,41 +1,44 @@
 import sys, os, shutil
 from configmanager import generateDomainConfigFile
 
-def showDefaultMessage():
+workingFolder = os.path.dirname(os.path.realpath(__file__))
+domainsFolder = workingFolder + "/../encryptbot_domains"
+
+def showDefaultMessage(flags, params):
     print("Use \"encryptbot.py help\" to get help.")
     
-def displayHelp():
+def displayHelp(flags, params):
     print("Usage: encryptbot [command] [flags] [parameters]")
     print("\nAvailable commands:")
-    print("help - Displays this information")
-    print("update - Checks if a new version of encryptbot is available and updates if so")
+    print("help          - Displays this information")
+    print("update        - Checks if a new version of encryptbot is available and updates if so")
     print("create-domain - Creates a new domain to be managed by encryptbot (use before get-certs and revoke-certs)")
     print("    Example: encryptbot.py create-domain yourdomain1.com yourdomain2.net")
-    print("get-certs - Obtains certificates for the domains specified in [parameters]")
+    print("get-certs     - Obtains certificates for the domains specified in [parameters]")
     print("    Example: encryptbot.py get-certs -f yourdomain1.com yourdomain2.net")
     print("    Example: encryptbot.py get-certs -a")
-    print("revoke-certs - Revokes a domain certificate")
+    print("revoke-certs  - Revokes a domain certificate")
     print("    Example: encryptbot.py revoke-certs yourdomain1.com")
     print("\nFlags:")
     print("-a - Obtain certificates for all registered domains")
     print("-f - Force certificate retrieval ignoring expiry checks (be wary of rate limits)")
 
-def checkForUpdates():
+def checkForUpdates(flags, params):
     pass
 
-def getCertificates():
+def getCertificates(flags, params):
     pass
 
-def revokeCertificates():
+def revokeCertificates(flags, params):
     pass
 
-def createDomainFolder():
-    pass
+def createDomainFolder(flags, params):
+    for domain in params:
+        os.mkdir(domainsFolder + "/" + domain)
+        generateDomainConfigFile(domainsFolder + "/" + domain)
+        print("Created domain " + domain)
 
 #####################################################################################################
-
-workingFolder = os.path.dirname(os.path.realpath(__file__))
-domainsFolder = workingFolder + "/../encryptbot_domains"
 
 # Generate the domains folder if it doesn't exist yet
 if not os.path.isdir(domainsFolder):
@@ -71,4 +74,4 @@ commands = {
     "create-domain": createDomainFolder
 }
 
-commands.get(command, showDefaultMessage)()
+commands.get(command, showDefaultMessage)(flags, params)
