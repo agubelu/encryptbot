c6ee3c339e7ad0fb9efd3e36e49ea84041333aaa
agubelu <agu@pqqqqw.pw>
2016-11-13 13:11:33 +0100

    Added shell=true to gen commands for Linux comp

--- bot/openssl.py
+++ bot/openssl.py
@@ -5,8 +5,8 @@ supported_algorithms = ["rsa", "prime256v1", "secp384r1"]
 
 def generateRSAkeypair(key_length):
     command = "openssl genpkey -algorithm RSA -out \"%saccount.key\" -pkeyopt rsa_keygen_bits:%s" % (getDomainsFolder(), key_length)
-    subprocess.call(command.strip(" "), stdout=None)
+    subprocess.call(command.strip(" "), stdout=None, shell=True)
     
 def generateECkeypair(algo):
     command = "openssl ecparam -name %s -genkey -noout -out \"%saccount.key\"" % (algo, getDomainsFolder())
-    subprocess.call(command.strip(" "), stdout=None)
+    subprocess.call(command.strip(" "), stdout=None, shell=True)
